44e28c5c7609b385a77f03559d45f5b4f36061cd
TheSharpen <sh4rp3n@gmail.com>
2023-04-09 23:57:03 +0200

    added NoteEvent class

---
+++ app/src/main/java/com/example/simplenotesapp/ui/NoteEvent.kt
@@ -0,0 +1,13 @@
+package com.example.simplenotesapp.ui
+
+import com.example.simplenotesapp.data.entity.Note
+
+sealed interface NoteEvent {
+
+    object SaveNote: NoteEvent
+    data class SetTitle(val title: String): NoteEvent
+    data class SetContent(val content: String): NoteEvent
+    object ShowDialog: NoteEvent
+    object HideDialog: NoteEvent
+    data class DeleteNote(val note: Note): NoteEvent
+}
