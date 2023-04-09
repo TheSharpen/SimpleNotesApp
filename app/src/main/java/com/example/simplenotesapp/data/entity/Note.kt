b2720913a5854d147d39502d914218689eab7df4
TheSharpen <sh4rp3n@gmail.com>
2023-04-09 23:50:06 +0200

    added Note data class (entity)

---
+++ app/src/main/java/com/example/simplenotesapp/data/entity/Note.kt
@@ -0,0 +1,14 @@
+package com.example.simplenotesapp.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity //tale name defaults to class name, in this case table name = Note
+data class Note (
+
+    val title: String,
+    val content: String,
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+)
