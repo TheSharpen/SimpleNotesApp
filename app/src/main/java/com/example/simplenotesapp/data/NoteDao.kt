a12d5aac9d8c027bb0b1043c3647acd225f50c41
TheSharpen <sh4rp3n@gmail.com>
2023-04-09 23:50:50 +0200

    added NoteDao

--- app/src/main/java/com/example/simplenotesapp/data/NoteDao.kt
+++ app/src/main/java/com/example/simplenotesapp/data/NoteDao.kt
@@ -1,3 +1,25 @@
 package com.example.simplenotesapp.data
 
-interface NoteDao {}
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import com.example.simplenotesapp.data.entity.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Upsert
+    suspend fun upsertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM note ORDER BY id ASC")
+    fun getAllNotes(): Flow<List<Note>>
+
+
+
+
+}
