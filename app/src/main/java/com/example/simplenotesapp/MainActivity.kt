2bd8718e189d587ad6297545dd8c24878b14ef16
TheSharpen <sh4rp3n@gmail.com>
2023-04-10 01:05:21 +0200

    Main activity updated

--- app/src/main/java/com/example/simplenotesapp/MainActivity.kt
+++ app/src/main/java/com/example/simplenotesapp/MainActivity.kt
@@ -3,27 +3,52 @@ package com.example.simplenotesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.example.simplenotesapp.data.NoteDatabase
+import com.example.simplenotesapp.ui.NoteScreen
+import com.example.simplenotesapp.ui.NoteViewModel
 import com.example.simplenotesapp.ui.theme.SimpleNotesAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val db by lazy {
+        Room.databaseBuilder(
+                applicationContext,
+                NoteDatabase::class.java,
+                "notes.db"
+        ).build()
+    }
+
+    private  val viewModel  by viewModels<NoteViewModel>(
+            factoryProducer = {
+                object: ViewModelProvider.Factory {
+                    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                        return NoteViewModel(db.dao) as T
+                    }
+                }
+            }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SimpleNotesAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
+                val state by viewModel.state.collectAsState()
+
+                NoteScreen(state = state, onEvent = viewModel::onEvent )
             }
         }
     }
