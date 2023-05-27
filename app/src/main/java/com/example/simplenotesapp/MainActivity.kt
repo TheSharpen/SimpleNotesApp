dd9c6a884764ef49072423930cdccc8c708ae3c6
TheSharpen <sh4rp3n@gmail.com>
2023-05-27 11:12:49 +0200

    MainACtivity.kt fullscreen added

--- app/src/main/java/com/example/simplenotesapp/MainActivity.kt
+++ app/src/main/java/com/example/simplenotesapp/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavType
@@ -22,51 +29,38 @@ import com.example.simplenotesapp.ui.splash.SplashScreen
 import com.example.simplenotesapp.ui.theme.ComposeCustomThemingTheme
 import com.example.simplenotesapp.util.Routes
 
+
 class MainActivity : ComponentActivity() {
 
     private val db by lazy {
         Room.databaseBuilder(
-                applicationContext,
-                NoteDatabase::class.java,
-                "notes.db"
+                applicationContext, NoteDatabase::class.java, "notes.db"
         ).build()
     }
 
 
-    private  val viewModel  by viewModels<NoteViewModel>(
-            factoryProducer = {
-                object: ViewModelProvider.Factory {
-                    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                        return NoteViewModel(db.dao) as T
-                    }
-                }
+    private val viewModel by viewModels<NoteViewModel>(factoryProducer = {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return NoteViewModel(db.dao) as T
             }
-    )
+        }
+    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//            WindowCompat.setDecorFitsSystemWindows(window, false)
-//            window.insetsController?.let { controller ->
-//                controller.hide(android.view.WindowInsets.Type.statusBars())
-//                controller.systemBarsBehavior =
-//                    android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-//            }
-//        } else {
-//            window.decorView.systemUiVisibility =
-//                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//        }
-
         setContent {
+
             ComposeCustomThemingTheme() {
+                TransparentStatusBar()
 
                 val state by viewModel.state.collectAsState()
                 val navController = rememberNavController()
 
-                NavHost(navController,
-                        startDestination = Routes.SPLASH_SCREEN) {
+                NavHost(
+                        navController, startDestination = Routes.SPLASH_SCREEN
+                ) {
                     composable(
                             Routes.NOTE_SCREEN
                     ) {
@@ -92,8 +86,8 @@ class MainActivity : ComponentActivity() {
                                     navArgument("content") { type = NavType.StringType })
                     ) { backStackEntry ->
                         val id = backStackEntry.arguments?.getInt("id") ?: 0
-                        val  title = backStackEntry.arguments?.getString("title") ?: ""
-                        val  content = backStackEntry.arguments?.getString("content") ?: ""
+                        val title = backStackEntry.arguments?.getString("title") ?: ""
+                        val content = backStackEntry.arguments?.getString("content") ?: ""
 
                         AddNoteDetailScreen(
                                 state = state,
@@ -114,5 +108,17 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun TransparentStatusBar() {
+    val context = LocalContext.current
+    val window = (context as ComponentActivity).window
 
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+
+    val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+    windowInsetsController?.hide(WindowInsetsCompat.Type.statusBars())
+    windowInsetsController?.systemBarsBehavior =
+        WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    window.statusBarColor = Color.Transparent.toArgb()
+}
 
